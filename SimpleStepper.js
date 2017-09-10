534dee0dd0e7c7528f08df33dad3123bd13b89e5
Himanshu Verma <himanshu.v09@gmail.com>
2017-09-10 23:23:43 +0530

    Update SimpleStepper.js
    Added prop type for latest React Native Version.

--- SimpleStepper.js
+++ SimpleStepper.js
@@ -1,5 +1,6 @@
-import React, {Component, PropTypes} from 'react';
+import React, {Component} from 'react';
 import {StyleSheet, Text, TouchableOpacity, Image, View} from 'react-native';
+import PropTypes from 'prop-types';
 
 export default class SimpleStepper extends Component {
   static propTypes = {
